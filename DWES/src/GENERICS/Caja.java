59c06750676694c1c8c12c5e4b7f55d4e197872a
Ivan <ivanjl4444@hotmail.com>
2019-09-30 16:58:44 +0200

    Caja acabado

--- DWES/src/GENERICS/Caja.java
+++ DWES/src/GENERICS/Caja.java
@@ -1,6 +1,26 @@
 package GENERICS;
 
-public class Caja {
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Caja<E extends String> {
 	
+	List <String> lista = new ArrayList<String>();
+
+	
+	private E object;
+	Caja(){
+		this.object=null;
+	}
+	
+	public int tamany() {
+		return this.object.length();
+		
+	}
+	public E getCaja() {
+		return this.object;
+	}
+
 
 }

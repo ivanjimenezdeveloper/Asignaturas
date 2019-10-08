4a4f9811fcd3e815fdc01e71652c0720fdfb0979
Ivan <ivanjl4444@hotmail.com>
2019-10-08 16:29:43 +0200

    SERVIDOR AC 1.1 comentarios / getPage (con syso)

--- DWES/src/Actividad1T1/Paginable.java
+++ DWES/src/Actividad1T1/Paginable.java
@@ -1,5 +1,8 @@
 package Actividad1T1;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
@@ -17,6 +20,7 @@ public class Paginable<T extends Producto> {
 	/*
 	 * Defino el tipo de lista que voy a usar
 	 */
+	
 	public Paginable() {
 		l = new ArrayList<T>();
 	}
@@ -25,9 +29,15 @@ public class Paginable<T extends Producto> {
 	 * Metodo para añadir productos
 	 * @param producto producto a añadir
 	 */
+	
 	public void add(T producto) {
 		l.add(producto);
 	}
+	
+	/**
+	 * Borra un producto en la lista
+	 * @param producto producto a borrar
+	 */
 	
 	public void remove(T producto) {
 		
@@ -35,57 +45,112 @@ public class Paginable<T extends Producto> {
 		
 	}
 	
+	/**
+	 *  Boolean que detecta si existe o no el producto
+	 * @param Producto producto a comprobar su existencia
+	 * @return devuelve el valor boolean
+	 */
+	
 	public boolean contains(T Producto) {
 		
 		
 		return l.contains(Producto);
 	}
 	
-	public int size() {
-		
 	
+	/**
+	 * Metodo para conocer el tamaño de la lista
+	 * @return devuelve el valor int del tamaño de la lista
+	 */
+	
+	public int size() {
 		
 		return l.size();
 		
 	}
+	
+	
+	/**
+	 * Encuentra la cantidad de paginas que tiene la lista segun
+	 * los productos por pagina y el tamaño de la misma
+	 * @return devuelve la cantidad de paginas totales en la lista
+	 */
 	
 	public int getTotal() {
 		
 		return l.size()/productoPagina +  (l.size()%productoPagina != 0? 1 : 0);
 		
 	}
+
 	
-	public void mostrar() {
-		
-		for(Producto p : l) {
-			System.out.println("Nombre: " + p.getNombre());
-			System.out.println("Valor: " + p.getPrecio());
-		}
-	}
+//	public void getPage(int index) {
+//
+//		int indiceInicial = index*productoPagina-productoPagina;
+//		int indiceFinal = index*productoPagina;
+//		if(index > this.getTotal() || index <= 0 ) {
+//			
+//			System.out.println("Excede el numero de páginas totales"
+//					+ " o es un numero menor o igual a 0");
+//			
+//		}else {
+//			
+//			for(int i =indiceInicial; i < indiceFinal; i++) {
+//				if(i == l.size()) {
+//					break;
+//				}
+//				System.out.println("Nombre: " + l.get(i).getNombre());
+//				System.out.println("Precio: " +	l.get(i).getPrecio());
+//				System.out.println(i);
+//				
+//			}
+//			
+//		}
+//		
+//		
+//	}
 	
-	public int getPage(int index) {
-		if(index > l.size()) {
+	public ArrayList<T> getPage(int index) {
+
+		ArrayList<T> objetos = new ArrayList<T>();
+		int indiceInicial = index*productoPagina-productoPagina;
+		int indiceFinal = index*productoPagina;
+		if(index > this.getTotal() || index <= 0 ) {
 			
-			return -1;
+			System.out.println("Excede el numero de páginas totales"
+					+ " o es un numero menor o igual a 0");
 			
 		}else {
 			
-			return index//conseguir el total de paginas y redondear hacia arriba 
+			for(int i =indiceInicial; i < indiceFinal; i++) {
+				if(i == l.size()) {
+					break;
+				}
+				System.out.println("Nombre: " + l.get(i).getNombre());
+				System.out.println("Precio: " +	l.get(i).getPrecio());
+//				System.out.println(i);
+				
+				objetos.add(l.get(i));
+				
+			}
 			
 		}
+		return objetos;
+		
+		
 	}
+	
 	
 	
 	/**
 	 * 
-	 * 
+	 * Collections.sort(l);
 	 * 		metodos 
 	 * 
 	 * 
 	 * 
 	 * getPage(int index)
-	 * 
-	 * 
+	 * Ordenar la lista
+	 * metodo para segun un objeto conseguir la pagina
 	 *
 	 */
 }

2c7d4f6db1ec18487b953e09044c5d230400e0cc
Ivan <ivanjl4444@hotmail.com>
2019-10-30 19:34:53 +0100

    Creando una sesion con cookies

--- Cookies/src/Sesion.java
+++ Cookies/src/Sesion.java
@@ -1,13 +1,15 @@
 
-
 import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.tomcat.util.http.Cookies;
 
 /**
  * Servlet implementation class Sesion
@@ -19,69 +21,87 @@ public class Sesion extends HttpServlet {
 	private PrintWriter out;
 	private String user;
 	private String passwd;
-	
+
 	private final String USERFINAL = "ricardo";
 	private final String PASSWDFINAL = "milos";
-    /**
-     * Default constructor. 
-     */
-    public Sesion() {
-            }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * Default constructor.
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public Sesion() {
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html");
 		out = response.getWriter();
-		
-		out.append("<html>")
-		.append("<body><h1>HOLA USUARIO DESCONOCIDO</h1>")
-		.append("<form method='POST' action='Sesion'>")
-		.append("<p>USUARIO</p><input type='text' name='usuario'>")
-		.append("<p>CONTRASENIA</p><input type='text' name='passwd'> ")
-		.append("<button>DALE</button>")
-		.append("</form>")
-		.append("</body></html>");
-		
-		
-		
-		
+		boolean logged = false;
+		String user = "";
+
+		Cookie[] ck = request.getCookies();
+
+		if (ck != null) {
+			for (int i = 0; i < ck.length; i++) {
+
+				if (ck[i].getName().contentEquals("usuario")) {
+					logged = true;
+					user = ck[i].getValue();
+
+				}
+
+			}
+		}
+
+		if (logged == true) {
+			out.append("<html>")
+					.append("<body><h1>HOLA USUARIO " + user + "</h1>")
+					.append("<form method='GET' action='UsuarioLogOut'>")
+					.append("<input type='submit' value='Log Out'>")
+					.append("</form>")
+					.append("</body></html>");
+
+		} else {
+			out.append("<html>")
+					.append("<body><h1>HOLA USUARIO DESCONOCIDO</h1>")
+					.append("<form method='GET' action='UsuarioLog'>")
+					.append("<input type='submit' value='Log In'>")
+					.append("</form>")
+					.append("</body></html>");
+
+		}
+
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	user = request.getParameter("usuario");
-	passwd = request.getParameter("passwd");
-	out.flush();
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+//		user = request.getParameter("usuario");
+//		passwd = request.getParameter("passwd");
+//		out.flush();
+//
+//		if (!user.equals(user) && !passwd.equals(passwd)) {
+//
+//			out.append("<html>").append("<body><h1>HOLA USUARIO DESCONOCIDO</h1>")
+//					.append("<form method='POST' action='Sesion'>")
+//					.append("<p>USUARIO</p><input type='text' name='usuario'>")
+//					.append("<p>CONTRASENIA</p><input type='text' name='passwd'>").append("<button>DALE</button>")
+//					.append("USUARIO NO ENCONTRADO").append("</form>").append("</body></html>");
+//
+//		} else {
+//			out.append("<html>").append("<body><h1>HOLA USUARIO DESCONOCIDO</h1>")
+//					.append("<form method='POST' action='Sesion'>")
+//					.append("<p>USUARIO</p><input type='text' name='usuario'>")
+//					.append("<p>CONTRASENIA</p><input type='text' name='passwd'> ").append("<button>DALE</button>")
+//					.append("FUNCIONA").append("</form>").append("</body></html>");
+//		}
 
-	if(!user.equals(user) && !passwd.equals(passwd)) {
-		
-		
-		out.append("<html>")
-		.append("<body><h1>HOLA USUARIO DESCONOCIDO</h1>")
-		.append("<form method='POST' action='Sesion'>")
-		.append("<p>USUARIO</p><input type='text' name='usuario'>")
-		.append("<p>CONTRASENIA</p><input type='text' name='passwd'>")
-		.append("<button>DALE</button>")
-		.append("USUARIO NO ENCONTRADO")
-		.append("</form>")
-		.append("</body></html>");
-		
-		
-	}else {
-		out.append("<html>")
-		.append("<body><h1>HOLA USUARIO DESCONOCIDO</h1>")
-		.append("<form method='POST' action='Sesion'>")
-		.append("<p>USUARIO</p><input type='text' name='usuario'>")
-		.append("<p>CONTRASENIA</p><input type='text' name='passwd'> ")
-		.append("<button>DALE</button>")
-		.append("FUNCIONA")
-		.append("</form>")
-		.append("</body></html>");
-	}
-	
 	}
 
 }
--- Cookies/src/UsuarioLog.java
+++ Cookies/src/UsuarioLog.java
@@ -1,8 +1,10 @@
 
-
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,29 +15,61 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/UsuarioLog")
 public class UsuarioLog extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public UsuarioLog() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public UsuarioLog() {
+		super();
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String error = request.getParameter("error");
+		PrintWriter out;
+		
+		response.setContentType("text/html");
+		
+		out = response.getWriter();
+		
+		out.append("<form method='POST' action='UsuarioLog'>")
+		.append("<p>USUARIO</p>")
+		.append("<input type='text' name='usuario'>")
+		.append("<p>Contrasenia</p>")
+		.append("<input type='password' name='pass'>")
+		.append("<input type='submit' value='Log In'>")
+		.append("</form>");
+		
+		if(error != null) {
+			out.append("<p>ERROR KRITICO</p>");
+		}
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+	String usuario = request.getParameter("usuario");
+	String password = request.getParameter("pass");
+	
+	if(usuario != null && password != null && usuario.equals("ivan")) {
+		Cookie c = new Cookie("usuario", usuario);
+		response.addCookie(c);
+		response.sendRedirect("Sesion");
+	}else {
+		response.sendRedirect("UsuarioLog?error=1");
+	}
+	
+	
+	
 	}
 
 }

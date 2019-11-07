9275702e74a89a2a8c1fe0facf7796ee51602286
ivan <ivanjl4444@hotmail.com>
2019-11-07 15:48:07 +0100

    html base mostrar simulaciones y css

--- Hipotecas/src/MostrarSimulaciones.java
+++ Hipotecas/src/MostrarSimulaciones.java
@@ -1,11 +1,13 @@
 
-
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class MostrarSimulaciones
@@ -13,26 +15,50 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/MostrarSimulaciones")
 public class MostrarSimulaciones extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public MostrarSimulaciones() {
-        super();
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+	public MostrarSimulaciones() {
+		super();
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		PrintWriter out = response.getWriter();
+		boolean logged = false;
+		String user = "";
+
+		// Crea la sesion
+		HttpSession sesion = request.getSession(true);
+
+		// Coge el atributo de la sesion usuario
+		user = (String) sesion.getAttribute("usuario");
+
+		// En caso de que el usuario recogido no este vacio significara que hay alguien
+		// logged
+		// por lo que se cambiara el boolean logged a true y mostrare el html
+		// para usuarios loggeds
+		if (user != null && !user.contentEquals("")) {
+			logged = true;
+		}
+//en caso de no estar logged redirige a el html para usuarios no logged
+		if (logged == true) {
+
+		} else {
+			response.sendRedirect("MainNoLogged.html");
+
+		}
 
 	}
 
+	
+	protected void mostrarSimulaciones() {
+		
+	}
 }

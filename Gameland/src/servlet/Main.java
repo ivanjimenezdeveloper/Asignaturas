dd6591585b8453072d71e7c3c1523d250813de74
Ivan <ivanjl4444@hotmail.com>
2019-11-11 20:48:43 +0100

    ejercicio 3 y 4 DWEC y inicio MAIN gameland

--- Gameland/src/servlet/Main.java
+++ Gameland/src/servlet/Main.java
@@ -6,6 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class Main
@@ -13,20 +14,37 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Main")
 public class Main extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
 
-    public Main() {
-        super();
-    }
-
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		
+	public Main() {
+		super();
 	}
 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		boolean logged = false;
+		String user = "";
+		
+		
+		//Creamos la sesion
+		HttpSession sesion = request.getSession(true);
+		
+		//Coge el atributo usuario de la sesion
+		user = (String) sesion.getAttribute("usuario");
+		
+		/**
+		 * Si encuentra hay algo que se guarde en user significa que
+		 * hay un usuario Logged
+		 */
+		if(user != null && user.contentEquals("")) {
+			logged = true;
+		}
+		
+		
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
 	}
 

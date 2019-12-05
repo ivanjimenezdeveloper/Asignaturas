bcf1e9f8e3e22a6b14231b5ead8fb6a0e3b30279
Ivan <ivanjl4444@hotmail.com>
2019-12-05 20:48:12 +0100

    jiji

--- IMC/src/vista/nav/Nav.java
+++ IMC/src/vista/nav/Nav.java
@@ -16,7 +16,7 @@ public class Nav {
 	 */
 	public static void mostrar(PrintWriter out) {
 		String html = "            <div class=\"tab\">\n" + "                <ul>\n"
-				+ "                    <li><a href=\"Main\">SEARCH</a></li>\n"
+				+ "                    <li><a href=\"Main\">INICIO</a></li>\n"
 				+ "                    <li><a href=\"\">TOP Games</a></li>\n"
 				+ "                    <li><a href=\"MostarPorGenero\">By Genre</a></li>\n"
 				+ "                    <li> |</li>\n"

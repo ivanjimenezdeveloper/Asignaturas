671682d0897f9068c1f98bb379e199e7c9bf1ac2
Ivan <ivanjl4444@hotmail.com>
2019-12-05 19:56:02 +0100

    MAS COMENTARIOS

--- IMC/src/controlador/Main.java
+++ IMC/src/controlador/Main.java
@@ -20,11 +20,19 @@ public class Main extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
+		/*
+		 * TENER UN TOMEE VERSION 9
+		 * <Resource name="jdbc/IMC" auth="Container" type="javax.sql.DataSource"
+		 * maxActive="100" maxIdle="30" maxWait="10000" username="imcivan" password=
+		 * "Ageofempires2" driverClassName="com.mysql.cj.jdbc.Driver" url=
+		 * "jdbc:mysql://localhost:3306/IMC?useUnicode=true&amp;useJDBCCompliantTimezoneShift=true&amp;useLegacyDatetimeCode=false&amp;serverTimezone=UTC&amp;useSSL=FALSE"
+		 * />
+		 */
 		Usuario us = new Usuario();
 		us.setCorreo("EL CORREO");
 		Cabecera.mostrarLogged(response.getWriter(), us);
 		Nav.mostrar(response.getWriter());
-		Container.mostrar(response.getWriter());	
+		Container.mostrar(response.getWriter());
 		Footer.mostrar(response.getWriter());
 
 	}
